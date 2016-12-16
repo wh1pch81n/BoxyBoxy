a30439d31c5e80128347d63663bae782a1875eb0
Derrick Ho <derrickho@derricks-mbp.fios-router.home>
2016-12-16 00:17:51 -0500

    - fixing old unit tests

--- BoxyBoxxy/AppDelegate.swift
+++ BoxyBoxxy/AppDelegate.swift
@@ -19,11 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		return true
 	}
 
-	func applicationDidBecomeActive(_ application: UIApplication) {
-		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-	}
-
-
 
 }
 
